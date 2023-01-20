942836bc2aef145d0fefa60212718b8fe5a20f38
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-01-20 20:50:49 +0530

    Update country.py

--- Himawari/modules/country.py
+++ Himawari/modules/country.py
@@ -1,12 +1,9 @@
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
-from telethon.utils import get_input_location
 import flag
-import html, os
 from countryinfo import CountryInfo
-from Himawari import telethn as borg
-from Himawari.events import register
+
+from Himawari import dispatcher
+from Himawari  import telethn as borg
+from Himawari .events import register
 
 
 @register(pattern="^/country (.*)")
@@ -17,110 +14,106 @@ async def msg(event):
     lol = input_str
     country = CountryInfo(lol)
     try:
-	    a = country.info()
+        a = country.info()
     except:
-	    await event.reply("Country Not Avaiable Currently")
+        await event.reply("Country Not Available Currently")
     name = a.get("name")
-    bb= a.get("altSpellings")
-    hu = ''
+    bb = a.get("altSpellings")
+    hu = ""
     for p in bb:
-    	hu += p+",  "
-	
+        hu += p + ",  "
+
     area = a.get("area")
     borders = ""
     hell = a.get("borders")
     for fk in hell:
-	    borders += fk+",  "
-	
-    call = "" 
+        borders += fk + ",  "
+
+    call = ""
     WhAt = a.get("callingCodes")
     for what in WhAt:
-	    call+= what+"  "
-	
+        call += what + "  "
+
     capital = a.get("capital")
     currencies = ""
     fker = a.get("currencies")
     for FKer in fker:
-	    currencies += FKer+",  "
+        currencies += FKer + ",  "
 
     HmM = a.get("demonym")
     geo = a.get("geoJSON")
     pablo = geo.get("features")
     Pablo = pablo[0]
     PAblo = Pablo.get("geometry")
-    EsCoBaR= PAblo.get("type")
+    EsCoBaR = PAblo.get("type")
     iso = ""
     iSo = a.get("ISO")
     for hitler in iSo:
-      po = iSo.get(hitler)
-      iso += po+",  "
+        po = iSo.get(hitler)
+        iso += po + ",  "
     fla = iSo.get("alpha2")
     nox = fla.upper()
     okie = flag.flag(nox)
 
     languages = a.get("languages")
-    lMAO=""
+    lMAO = ""
     for lmao in languages:
-	    lMAO += lmao+",  "
+        lMAO += lmao + ",  "
 
     nonive = a.get("nativeName")
     waste = a.get("population")
     reg = a.get("region")
     sub = a.get("subregion")
     tik = a.get("timezones")
-    tom =""
+    tom = ""
     for jerry in tik:
-	    tom+=jerry+",   "
+        tom += jerry + ",   "
 
     GOT = a.get("tld")
     lanester = ""
     for targaryen in GOT:
-	    lanester+=targaryen+",   "
+        lanester += targaryen + ",   "
 
     wiki = a.get("wiki")
 
     caption = f"""<b><u>Information Gathered Successfully</b></u>
-<b>
-Country Name:- {name}
-Alternative Spellings:- {hu}
-Country Area:- {area} square kilometers
-Borders:- {borders}
-Calling Codes:- {call}
-Country's Capital:- {capital}
-Country's currency:- {currencies}
-Country's Flag:- {okie}
-Demonym:- {HmM}
-Country Type:- {EsCoBaR}
-ISO Names:- {iso}
-Languages:- {lMAO}
-Native Name:- {nonive}
-population:- {waste}
-Region:- {reg}
-Sub Region:- {sub}
-Time Zones:- {tom}
-Top Level Domain:- {lanester}
-wikipedia:- {wiki}</b>
 
-Gathered By @power_superbot.</b>
+<b>Country Name :</b> {name}
+<b>Alternative Spellings :</b> {hu}
+<b>Country Area :</b> {area} square kilometers
+<b>Borders :</b> {borders}
+<b>Calling Codes :</b> {call}
+<b>Country's Capital :</b> {capital}
+<b>Country's currency :</b> {currencies}
+<b>Country's Flag :</b> {okie}
+<b>Demonym :</b> {HmM}
+<b>Country Type :</b> {EsCoBaR}
+<b>ISO Names :</b> {iso}
+<b>Languages :</b> {lMAO}
+<b>Native Name :</b> {nonive}
+<b>Population :</b> {waste}
+<b>Region :</b> {reg}
+<b>Sub Region :</b> {sub}
+<b>Time Zones :</b> {tom}
+<b>Top Level Domain :</b> {lanester}
+<b>Wikipedia :</b> {wiki}
+
+<u>Information Gathered By {dispatcher.bot.first_name}</u>
 """
-    
-    
+
     await borg.send_message(
         event.chat_id,
         caption,
         parse_mode="HTML",
+        link_preview=None,
     )
-    
-    await event.delete()
 
 
 __help__ = """
-*Country* 
 
-*Commands*
-- /country [country name]*:* Gathering info about given country
+I will give information about a country
 
-@Shinobu_Update_Channel
+ ❍ /country <country name>*:* Gathering info about given country
 """
 
-__mod_name__ = "Country"
+__mod_name__ = "Cᴏᴜɴᴛʀʏ"
